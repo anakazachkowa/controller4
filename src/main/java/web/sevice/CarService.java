9253d8de9a23f5d8e41ddecbfb1eb347ae74b5ca
anakazachkowa <anakazachkowa@yandex.ru>
2023-06-18 21:03:36 +0400

    hi

---
+++ src/main/java/web/sevice/CarService.java
@@ -0,0 +1,10 @@
+package web.sevice;
+
+import web.Model.Car;
+
+import java.util.List;
+
+public interface CarService {
+           List<Car> carList(Integer count);
+    }
+
---
+++ src/main/java/web/sevice/CarServiceImpl.java
@@ -0,0 +1,22 @@
+package web.sevice;
+
+import web.Model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarServiceImpl implements CarService{
+   @Override
+    public List<Car> carList(Integer count) {
+      List<Car> carList; {
+            carList = new ArrayList<>();
+            carList.add(new Car("Volga", "LKH", 1999));
+            carList.add(new Car("Lada", "NHJ", 2001));
+            carList.add(new Car("Niva", "KUI", 2005));
+            carList.add(new Car("Mazda", "MNG", 2022));
+            carList.add(new Car("Gazal", "NHG", 2007));
+        }
+       return carList.stream().limit(count).collect(Collectors.toList());
+   }
+}
